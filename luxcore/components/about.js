13693c5f3826635027029e7bf5228053fc69474c
cdasilvasantos <cd58@njit.edu>
2023-12-16 20:22:13 -0500

    added about card, missing card with slideshow

--- luxcore/components/about.js
+++ luxcore/components/about.js
@@ -1,16 +1,22 @@
-export default function Hero() {
+import Image from 'next/image';
+
+export default function About() {
     return(
         <main>
-        <section class="banner">
-            <section class="calltoaction">
-            <h1>Where culinary craftsmanship meetes timeless elegance</h1>
-            <p>Join our members club for exclusive offers.</p>
-            <section class="button-container">
-                <a href="#project-section" class="button-right">Members Club</a>
-            </section>
-            </section>
-        </section>
-    </main>
+                <div class="project-cards">
+                    <div class="project1">
+                        <p class="section-top">The bright new experience in the heart of newark, new jersey</p>
+                        <h2>Luxcore Steakhouse</h2>
+                        <button class="btn about">About Us</button>
+                        <button class="btn members">Members Club</button>
+                        <p class="about-text">At LuxCore Steakhouse , we  strive to craft an exquisite haven where you and your cherished companions indulge in moments of serenity. With unwavering dedication, we curate an ambiance where each visit becomes a plate of memories, and our culinary artistry, an unparalleled testament to excellence. Welcome to a sanctuary of fine dining, where every dish is a masterpiece and every experience is etched in the elegance of shared stories.</p>
+                    </div>
 
+                    <div class="project2">
+                        
+                    </div>
+
+                </div>
+    </main>
     )
 } 
--- luxcore/src/app/page.js
+++ luxcore/src/app/page.js
@@ -1,13 +1,16 @@
 import Navbar from "../../components/navbar";
 import Hero from "../../components/hero";
+import About from "../../components/about";
 import Menu from "../../components/menu";
 import Footer from "../../components/footer"
 
 export default function Home() {
   return (
     <main>
+      <title>Luxcore</title>
       <Navbar/>
       <Hero />
+      <About />
       <Menu />
       <Footer />
     </main>
