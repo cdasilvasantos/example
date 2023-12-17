25d1e58f79d5a8df2aee3d66875b3e823b5bbd5a
cdasilvasantos <cd58@njit.edu>
2023-12-16 20:41:23 -0500

    added footer details

--- luxcore/components/footer.js
+++ luxcore/components/footer.js
@@ -1,6 +1,14 @@
 export default function Footer() {
     return (
         <footer>
+            <h1>Luxcore</h1>
+            <h3>123 Warren Street, Newark, NJ | 123-456-7890 </h3>
+            <button class="btn about1">About Us</button>
+            <button class="btn menu">Menu</button>
+            <button class="btn contact">Contact Us</button>
+            <button class="btn members2">Members Club</button>
+            
+            <hr></hr>
         <p>&copy; 2023 LuxCore. All Rights Reserved.</p>
     </footer>
     )
