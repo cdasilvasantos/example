e8dba187f5e6a597d807170dc3741c51275b5a73
cdasilvasantos <cd58@njit.edu>
2023-12-17 14:58:44 -0500

    added menu part

--- luxcore/components/menu.js
+++ luxcore/components/menu.js
@@ -1,9 +1,23 @@
+import Image from "next/image"
+import steak from "../public/images/steak.png"
+
 export default function Menu() {
     return(
         <main>
+        <section class="discover">
+            <h1>Discover Menu</h1>
+        </section>
         <section class="steak">
-            <h2>World Class Steak</h2>
+        <p class="section-top">World Class Steak</p>
             <p>At LuxCore Steakhouse, we strive to craft an exquisite haven where you and your cherished companions indulge in moments of serenity. With unwavering dedication, we curate an ambiance where each visit becomes a plate of memories, and our culinary artistry, an unparalleled testament to excellence. Welcome to a sanctuary of fine dining, where every dish is a masterpiece and every experience is etched in the elegance of shared stories.</p>
+            <div class="steak-image">
+                <Image
+                    src={steak}
+                    width={500}
+                    height={500}
+                    alt="Picture of the author"
+                />
+            </div>
         </section>
         <section class="wine">
             <h2>Exceptional Wine Selection</h2>
