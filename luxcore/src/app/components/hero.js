7898a52149a7ae4fc59d974645826ba71c6df420
cdasilvasantos <cd58@njit.edu>
2023-12-18 12:55:06 -0500

    fixed font for banner

--- luxcore/src/app/components/hero.js
+++ luxcore/src/app/components/hero.js
@@ -1,14 +1,17 @@
-export default function Hero() {
-    return(
-        <main>
-        <section class="banner">
-            <section class="calltoaction">
-            <h1>Where culinary craftsmanship meets timeless elegance</h1>
-            <p>Join our members club for exclusive offers.</p>
-            <button class="btn about1"> C&M Members Club</button>
-            </section>
-        </section>
-    </main>
+import React from 'react';
 
-    )
-} 
+export default function Hero() {
+    return (
+        <main>
+            <section className="banner">
+                <section className="calltoaction">
+                    <h1>Where culinary craftsmanship meets timeless elegance</h1>
+                    <p>Join our members club for exclusive offers.</p>
+                    <a href="Footer.js" className="btn about1">
+                        C&M Members Club
+                    </a>
+                </section>
+            </section>
+        </main>
+    );
+}
